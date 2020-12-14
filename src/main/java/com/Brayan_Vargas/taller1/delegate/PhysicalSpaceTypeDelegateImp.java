e912a100955b7f5f787041d1aca5c71d55148e58
david huertas <aletru321@gmail.com>
2020-12-14 12:46:33 -0500

    Physicalspaceimp and typesofspacesimp delegate added

---
+++ src/main/java/com/Brayan_Vargas/taller1/delegate/PhysicalSpaceTypeDelegateImp.java
@@ -0,0 +1,30 @@
+package com.Brayan_Vargas.taller1.delegate;
+
+import com.Brayan_Vargas.taller1.model.Physicalspacetype;
+
+public class PhysicalSpaceTypeDelegateImp implements PhysicalSpaceTypeDelegate{
+    @Override
+    public Physicalspacetype GET_Campus(long id) {
+        return null;
+    }
+
+    @Override
+    public void GET_Institutions() {
+
+    }
+
+    @Override
+    public Physicalspacetype POST_Campus(Physicalspacetype institutioncampus) {
+        return null;
+    }
+
+    @Override
+    public void PUT_Campus(Physicalspacetype institutioncampus) {
+
+    }
+
+    @Override
+    public void DELETE_Campus(Physicalspacetype institutioncampus) {
+
+    }
+}
