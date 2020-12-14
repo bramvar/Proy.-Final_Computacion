5ef3f38a5ec63c62c2fc5ef63e975d0400f57930
david huertas <aletru321@gmail.com>
2020-12-14 12:55:29 -0500

    fixed spaces delegate, minor issue

--- src/main/java/com/Brayan_Vargas/taller1/delegate/PhysicalSpaceDelegate.java
+++ src/main/java/com/Brayan_Vargas/taller1/delegate/PhysicalSpaceDelegate.java
@@ -3,10 +3,10 @@ package com.Brayan_Vargas.taller1.delegate;
 import com.Brayan_Vargas.taller1.model.Physicalspace;
 
 public interface PhysicalSpaceDelegate {
-    public Physicalspace GET_Campus(long id);
-    public void GET_Institutions();
-    public Physicalspace POST_Campus(Physicalspace institutioncampus);
-    public void PUT_Campus(Physicalspace institutioncampus);
-    public void DELETE_Campus(Physicalspace institutioncampus);
+    public Physicalspace GET_Space(long id);
+    public void GET_Space();
+    public Physicalspace POST_Space(Physicalspace institutioncampus);
+    public void PUT_Space(Physicalspace institutioncampus);
+    public void DELETE_Space(Physicalspace institutioncampus);
 
 }
--- src/main/java/com/Brayan_Vargas/taller1/delegate/PhysicalSpaceTypeDelegate.java
+++ src/main/java/com/Brayan_Vargas/taller1/delegate/PhysicalSpaceTypeDelegate.java
@@ -3,9 +3,9 @@ package com.Brayan_Vargas.taller1.delegate;
 import com.Brayan_Vargas.taller1.model.Physicalspacetype;
 
 public interface PhysicalSpaceTypeDelegate {
-    public Physicalspacetype GET_Campus(long id);
-    public void GET_Institutions();
-    public Physicalspacetype POST_Campus(Physicalspacetype institutioncampus);
-    public void PUT_Campus(Physicalspacetype institutioncampus);
-    public void DELETE_Campus(Physicalspacetype institutioncampus);
+    public Physicalspacetype GET_Type(long id);
+    public void GET_Type();
+    public Physicalspacetype POST_Type(Physicalspacetype institutioncampus);
+    public void PUT_Type(Physicalspacetype institutioncampus);
+    public void DELETE_Type(Physicalspacetype institutioncampus);
 }
